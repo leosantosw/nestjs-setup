4df36547f05135e6c1f587d69ca06349073f05e7
leosantosw <leonardosantosx0077@gmail.com>
2023-07-10 23:52:29 -0300

    chore: update config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(4000)
+
+  app.enableCors()
+  app.setGlobalPrefix('api')
+
+  const config = new DocumentBuilder()
+    .setTitle('Users API')
+    .setDescription('The Users API description')
+    .setVersion('1.0')
+    .addTag('Users')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api/docs', app, document)
+
+  const port = parseInt(process.env.PORT ?? '4000')
+
+  await app.listen(port, () => {
+    console.log(`🔥 Server running on port ${port}`)
+  })
 }
+
 bootstrap()
